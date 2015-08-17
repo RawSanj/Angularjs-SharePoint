bfe16af1ca13bd9d665853f2d29290791547e7ff
Sanjay Rawat <RawSanj@users.noreply.github.com>
2015-08-17 22:55:15 +0530

    Pagination now works.
    Need to fix the OrderBy Filter

--- New Project/script.js
+++ New Project/script.js
@@ -89,38 +89,34 @@ app.controller('PageCtrl', ['$scope', '$http', 'filterFilter', function ($scope,
 
         	});
 
+        
+
+            // create empty search model (object) to trigger $watch on update
+            $scope.search = {};
+
+            $scope.resetFilters = function () {
+                // needs to be a function or it won't trigger a $watch
+                $scope.search = {};
+            };
+
+            // pagination controls
+            $scope.currentPage = 1;
+            $scope.totalItems = $scope.items.length;
+            $scope.entryLimit = 50; // items per page
+            $scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
+
+            // $watch search to update pagination
+            $scope.$watch('search', function (newVal, oldVal) {
+                $scope.filtered = filterFilter($scope.items, newVal);
+                $scope.totalItems = $scope.filtered.length;
+                $scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
+                $scope.currentPage = 1;
+            }, true);
 
 
     	 });
 
     }, true);
-
-
-
-
-
-
-	// create empty search model (object) to trigger $watch on update
-	$scope.search = {};
-
-	$scope.resetFilters = function () {
-		// needs to be a function or it won't trigger a $watch
-		$scope.search = {};
-	};
-
-	// pagination controls
-	$scope.currentPage = 1;
-	$scope.totalItems = $scope.items.length;
-	$scope.entryLimit = 50; // items per page
-	$scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
-
-	// $watch search to update pagination
-	$scope.$watch('search', function (newVal, oldVal) {
-		$scope.filtered = filterFilter($scope.items, newVal);
-		$scope.totalItems = $scope.filtered.length;
-		$scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
-		$scope.currentPage = 1;
-	}, true);
 
 
 
